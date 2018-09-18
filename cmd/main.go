741977aaf8453e826e7d93bb33eb38485460c8b8
Zoltan Giber <zgiber@gmail.com>
2018-09-18 22:55:46 +0100

    adjust max speed as used in the test

--- cmd/main.go
+++ cmd/main.go
@@ -10,7 +10,7 @@ import (
 var (
 	input    = flag.String("in", "testpoints.csv", "path for the input .csv file")
 	output   = flag.String("out", "filtered.csv", "path for the filtered output .csv file")
-	maxSpeed = flag.Float64("vmax", 15, "maximum average speed (points which require speeds above this will be removed)")
+	maxSpeed = flag.Float64("vmax", 20, "maximum average speed in m/s (points which require speeds above this will be removed)")
 )
 
 func main() {
